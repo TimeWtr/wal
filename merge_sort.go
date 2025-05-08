ee130730ea52819eeca5ab8be4f15d2e68ef07c3
2301_79425991 <2301_79425991@noreply.gitcode.com>
2025-05-08 19:26:31 +0800

    feat: 运行make check

---
+++ merge_sort.go
@@ -0,0 +1,80 @@
+// Copyright 2025 TimeWtr
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wal
+
+import (
+	"fmt"
+	gl "github.com/bboreham/go-loser"
+	"math"
+)
+
+type list struct {
+	l   []uint64
+	cur uint64
+}
+
+func newList(l ...uint64) *list {
+	return &list{l: l}
+}
+
+func (it *list) At() uint64 {
+	return it.cur
+}
+
+func (it *list) Next() bool {
+	if len(it.l) > 0 {
+		it.cur = it.l[0]
+		it.l = it.l[1:]
+		return true
+	}
+	it.cur = 0
+	return false
+}
+
+func (it *list) Seek(val uint64) bool {
+	for it.cur < val && len(it.l) > 0 {
+		it.cur = it.l[0]
+		it.l = it.l[1:]
+	}
+	return len(it.l) > 0
+}
+
+type mergeSort struct {
+	tree    *gl.Tree[uint64, *list]
+	sources []*list
+}
+
+func newMergeSort(sources [][]uint64) *mergeSort {
+	ms := &mergeSort{}
+	ms.sources = make([]*list, len(sources))
+	for i := 0; i < len(sources); i++ {
+		ms.sources[i] = newList(sources[i]...)
+	}
+
+	ms.tree = gl.New[uint64](ms.sources, math.MaxUint64)
+	return ms
+}
+
+func (ms *mergeSort) Run() {
+	for ms.tree.Next() {
+		fmt.Println("at: ", ms.tree.At())
+	}
+}
+
+func (ms *mergeSort) close() {
+	ms.tree.Close()
+}
+
+func (ms *mergeSort) less(a, b uint64) bool { return a < b }
---
+++ merge_sort_test.go
@@ -0,0 +1,82 @@
+// Copyright 2025 TimeWtr
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wal
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+)
+
+func TestNewMergeSort(t *testing.T) {
+	sources := [][]uint64{
+		{1, 5, 9},
+		{2, 6, 11},
+		{3, 7},
+		{1, 2, 10, 11, 13, 19},
+		{},
+	}
+
+	ms := newMergeSort(sources)
+	defer ms.close()
+
+	ms.Run()
+}
+
+func TestNewMergeSort_Random(t *testing.T) {
+	sources := make([][]uint64, 12)
+	for i := 0; i < 10; i++ {
+		item := make([]int, 100)
+		for j := range item {
+			item[j] = rand.Intn(1000)
+		}
+		sort.Ints(item)
+
+		sourceItem := make([]uint64, 100)
+		for j, val := range item {
+			sourceItem[j] = uint64(val)
+		}
+		sources[i] = sourceItem
+	}
+
+	ms := newMergeSort(sources)
+	defer ms.close()
+
+	ms.Run()
+}
+
+func BenchmarkMergeSort_Run(b *testing.B) {
+	sources := make([][]uint64, 12)
+	for i := 0; i < 10; i++ {
+		item := make([]int, 1000)
+		for j := range item {
+			item[j] = rand.Intn(1000)
+		}
+		sort.Ints(item)
+
+		sourceItem := make([]uint64, 1000)
+		for j, val := range item {
+			sourceItem[j] = uint64(val)
+		}
+		sources[i] = sourceItem
+	}
+
+	ms := newMergeSort(sources)
+	defer ms.close()
+
+	for n := 0; n < b.N; n++ {
+		ms.Run()
+	}
+}
